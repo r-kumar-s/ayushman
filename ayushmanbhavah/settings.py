24d75709b9f42cf25554973c5171d0102d9205b2
Rohit K <rshaw@aecordigital.com>
2025-09-18 11:31:16 +0530

    SMTP settings ...

--- ayushmanbhavah/settings.py
+++ ayushmanbhavah/settings.py
@@ -152,10 +152,11 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = False               # True for 587
-EMAIL_USE_SSL = True 
+#EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST = 'mail.ayushmaanbhavah.com'
+EMAIL_PORT = 465
 EMAIL_HOST_USER = 'contact@ayushmaanbhavah.com'
 EMAIL_HOST_PASSWORD = 'Sushma123#'
-EMAIL_PORT = 465
