358b142f724665beca2c31ee72767d8a9c269a35
Rohit K <rshaw@aecordigital.com>
2025-09-18 11:42:44 +0530

    SMTP setting ...

--- ayushmanbhavah/settings.py
+++ ayushmanbhavah/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(p#g%$2%3v%gw!6gvk1l+lhw-evoi3y#u_dnl8@@jw6fq8nyfr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'www.ayushmaanbhavah.com',
