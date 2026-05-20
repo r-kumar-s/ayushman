1648c68da46c24ea78f4cc4b569fb6b8b036b017
Rohit K <rshaw@aecordigital.com>
2026-05-20 17:12:25 +0530

    Work in Patient details ...

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users/', views.users, name='users'),
-    path('create_users', views.create_users, name='create_users'),
-    ]
+
+    path('', views.users, name='users'),
+
+    path('create_users/', views.create_users, name='create_users'),
+
+    path('detail/<int:id>/', views.detail, name='detail'),
+
+    path('edit/<int:id>/', views.edit, name='edit'),
+
+    path('update/<int:id>/', views.update, name='update'),
+
+    path('delete/<int:id>/', views.delete, name='delete'),
+
+    path('create_with_followup/', views.create_with_followup, name='create_with_followup'),
+
+]
