700c1a1ca2c659417643f5c177aa17d874b8435f
Rohit K <rshaw@aecordigital.com>
2026-07-07 15:26:24 +0530

    SEO related changes ...

--- assets/js/type.js
+++ assets/js/type.js
@@ -1,5 +1,5 @@
 window.ityped.init(document.querySelector('.ityped'),{
-    strings: ['Panchakarma & Natural Treatment'],
+    strings: ['Trusted Ayurvedic Treatment'],
     typeSpeed: 150,
     backSpeed: 150,
     loop: true
