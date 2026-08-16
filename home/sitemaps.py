00c3fad222a8a9e7eea4d7224c089e64df70556b
Rohit K <rshaw@aecordigital.com>
2026-08-16 13:36:36 +0530

    Change sitemap.py ...

--- home/sitemaps.py
+++ home/sitemaps.py
@@ -1,40 +1,45 @@
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
 
+
 class StaticViewSitemap(Sitemap):
     protocol = 'https'
     changefreq = "weekly"
-    priority = 1.0
 
     def items(self):
         return [
-                'index',
-                'about',
-                'thyroid',
-                'gastrointestinal',
-                'lungs',
-                'hematological',
-                'stone',
-                'tumor',
-                'arthritis',
-                'skin',
-                'female-genital-organ',
-                'basti',
-                'nasya',
-                'raktamokshana',
-                'vamana',
-                'virechana',
-                'udarshodhak',
-                'contact',
-                'dr_sushma_tiwary', 
-                'shirodhara',
-                'abhyanga'
-            ]
+            'index',
+            'about',
+            'thyroid',
+            'gastrointestinal',
+            'lungs',
+            'hematological',
+            'stone',
+            'tumor',
+            'arthritis',
+            'skin',
+            'female-genital-organ',
+            'basti',
+            'nasya',
+            'raktamokshana',
+            'vamana',
+            'virechana',
+            'udarshodhak',
+            'contact',
+            'dr_sushma_tiwary',
+            'shirodhara',
+            'abhyanga',
+        ]
 
     def location(self, item):
+        # Homepage should be "/" instead of "/index"
+        if item == 'index':
+            return '/'
+
         return reverse(item)
 
     def priority(self, item):
         if item == 'index':
             return 1.0
+
         return 0.8
